3797dbd364f3e7788333bec9f1b1a869439ff59b
king12318 <106511757+king12318@users.noreply.github.com>
2023-05-04 18:34:29 +0700

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,42 @@
+import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+import './globals.css'
+import { Inter } from 'next/font/google'
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/modals/LoginModal'
+import getCurrentUser from './actions/getCurrentUser'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+const font = Nunito({
+  subsets: ["latin"], 
+})
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+         <ClientOnly>
+          <ToasterProvider/>
+          <LoginModal/>
+          <RegisterModal/>
+          <Navbar currentUser={currentUser}/>
+         </ClientOnly>
+        {children}
+        </body>
+    </html>
+  )
+}
